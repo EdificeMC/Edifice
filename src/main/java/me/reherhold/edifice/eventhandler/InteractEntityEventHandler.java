cdb37913b856815551d2f5230df77429d67fd4a3
Robert Herhold <reherhold@gmail.com>
2016-07-10 16:12:26 -0400

    WIP

--- src/main/java/me/reherhold/edifice/eventhandler/InteractEntityEventHandler.java
+++ src/main/java/me/reherhold/edifice/eventhandler/InteractEntityEventHandler.java
@@ -24,7 +24,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockSnapshot;
@@ -159,7 +158,7 @@ public class InteractEntityEventHandler {
             int length = structureJson.getInt(LENGTH);
             int height = structureJson.getInt(HEIGHT);
             Location<World> itemFrameLoc = itemFrame.getLocation();
-            Direction direction = itemFrame.direction().get();
+            Direction itemFrameDirection = itemFrame.direction().get();
             // Vector offset to be added to the item frame location to result in the
             // location of the block containing the StructureData
             Vector3i structureBaseOffset = new Vector3i(0, 0, 0);
@@ -169,7 +168,7 @@ public class InteractEntityEventHandler {
             // For each direction, we will put the block in the opposite direction
             // as the item frame, since the way the item frame "faces" is the side
             // away from the block it is on
-            switch (direction) {
+            switch (itemFrameDirection) {
                 case NORTH: // Towards negative z
                     structureBaseOffset = new Vector3i(0, 0, 1);
                     offset = new Vector3i(0, 0, 2);
@@ -199,12 +198,9 @@ public class InteractEntityEventHandler {
                 return;
             }
 
-            int rotationIterations =
-                    CARDINAL_SET.indexOf(direction.getOpposite()) - CARDINAL_SET.indexOf(Direction.valueOf(structureJson.getString(DIRECTION)));
-            if (rotationIterations < 0) {
-                rotationIterations += 4;
-            }
-            rotateBlocks(structureJson.getJSONArray(BLOCKS), rotationIterations);
+            // Difference in indices between the opposite of the way the item frame is facing (the intuitive direction) and the structure direction
+            int directionIndexDifference = CARDINAL_SET.indexOf(itemFrameDirection.getOpposite()) - CARDINAL_SET.indexOf(Direction.valueOf(structureJson.getString(DIRECTION)));
+            int rotationIterations = directionIndexDifference < 0 ? directionIndexDifference + 4 : directionIndexDifference;
 
             Vector3i originLocation = itemFrameLoc.getBlockPosition().add(offset);
 
@@ -215,50 +211,50 @@ public class InteractEntityEventHandler {
             // locations and world UUID
             structureJson.getJSONArray(BLOCKS).forEach((obj) -> {
                 JSONObject blockJson = (JSONObject) obj;
-                JSONObject pos = blockJson.getJSONObject(POSITION);
-                pos.put(POSITION_X, pos.getInt(POSITION_X) + originLocation.getX());
-                pos.put(POSITION_Y, pos.getInt(POSITION_Y) + originLocation.getY());
-                pos.put(POSITION_Z, pos.getInt(POSITION_Z) + originLocation.getZ());
-                blockJson.put(WORLD_UUID, itemFrameLoc.getExtent().getUniqueId().toString());
-
+                rotateBlockPosition(blockJson, rotationIterations);
+                restoreBlockLocation(blockJson, originLocation, itemFrameLoc.getExtent().getUniqueId());
+                
+                System.out.println(blockJson.toString());
+                
                 // Deserialize the JSON block into a BlockSnapshot
-                    BufferedReader reader = new BufferedReader(new StringReader(blockJson.toString()));
-                    JSONConfigurationLoader loader = JSONConfigurationLoader.builder().setSource(() -> {
-                        return reader;
-                    }).build();
-                    ConfigurationNode node = null;
-                    try {
-                        node = loader.load();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    Optional<BlockSnapshot> blockSnapOpt =
-                            Sponge.getDataManager().deserialize(BlockSnapshot.class, ConfigurateTranslator.instance().translateFrom(node));
-                    if (!blockSnapOpt.isPresent()) {
-                        plugin.getLogger()
-                                .error("There was an error deserializing to BlockSnapshot for structure with ID " + structureJson.getString(ID));
-                        player.sendMessage(Text.of(TextColors.RED, "There was an error deserializing the structure. Cannot continue"));
-                        return;
-                    }
-                    BlockSnapshot block = blockSnapOpt.get();
-                    Optional<HeldItemProperty> itemEquivalentOpt = block.getProperty(HeldItemProperty.class);
-                    ItemType itemType = null;
-                    if(itemEquivalentOpt.isPresent()) {
-                    	itemType = itemEquivalentOpt.get().getValue();
-                    } else if(BLOCK_ITEM_MAP.containsKey(block.getState().getType())) {
-                    	itemType = BLOCK_ITEM_MAP.get(block.getState().getType());
-                    } else {
-                    	return;
-                    }
-                    String itemId = itemType.getId();
-                    if (deserializedStructureBlocks.containsKey(itemId)) {
-                        deserializedStructureBlocks.get(itemId).add(block);
-                    } else {
-                        ArrayList<BlockSnapshot> newList = new ArrayList<BlockSnapshot>();
-                        newList.add(block);
-                        deserializedStructureBlocks.put(itemId, newList);
-                    }
-                });
+                Optional<BlockSnapshot> blockSnapOpt = deserializeBlock(blockJson);
+                
+				if (!blockSnapOpt.isPresent()) {
+					plugin.getLogger().error("There was an error deserializing to BlockSnapshot for structure with ID "
+							+ structureJson.getString(ID));
+					player.sendMessage(
+							Text.of(TextColors.RED, "There was an error deserializing the structure. Cannot continue"));
+					return;
+				}
+				BlockSnapshot block = blockSnapOpt.get();
+				
+				System.out.println("Deserialized block");
+				System.out.println(block.toString());
+				
+				block = rotateBlockDirectionData(block, rotationIterations);
+				
+				System.out.println("Rotated block");
+				System.out.println(block.toString());
+				
+				Optional<HeldItemProperty> itemEquivalentOpt = block.getProperty(HeldItemProperty.class);
+				ItemType itemType = null;
+				if (itemEquivalentOpt.isPresent()) {
+					itemType = itemEquivalentOpt.get().getValue();
+				} else if (BLOCK_ITEM_MAP.containsKey(block.getState().getType())) {
+					itemType = BLOCK_ITEM_MAP.get(block.getState().getType());
+				} else {
+					return;
+				}
+				String itemId = itemType.getId();
+				if (deserializedStructureBlocks.containsKey(itemId)) {
+					deserializedStructureBlocks.get(itemId).add(block);
+				} else {
+					ArrayList<BlockSnapshot> newList = new ArrayList<BlockSnapshot>();
+					newList.add(block);
+					deserializedStructureBlocks.put(itemId, newList);
+                }
+            });
+            System.out.println("Done");
             Structure structure =
                     new Structure(structureJson.getString(NAME), UUID.fromString(structureJson.getString(CREATOR_UUID)), player.getUniqueId(),
                             Direction.valueOf(structureJson
@@ -268,12 +264,14 @@ public class InteractEntityEventHandler {
                     (StructureDataManipulatorBuilder) Sponge.getDataManager().getManipulatorBuilder(StructureData.class).get();
             StructureData data = builder.createFrom(structure);
 
+            System.out.println(1);
             DataTransactionResult result = structureBlock.offer(data);
             if (!result.isSuccessful()) {
                 player.sendMessage(Text.of(TextColors.RED, "There was an error starting the construction."));
                 return;
             }
 
+            System.out.println(2);
             Text msg1 = Text.of(TextColors.GREEN, "You have started the construction of ", TextColors.GOLD, structure.getName());
             Text msg2 = Text.of(TextColors.GREEN, ".");
 
@@ -284,26 +282,68 @@ public class InteractEntityEventHandler {
             } catch (Exception e) {
             }
 
+            System.out.println(3);
             player.sendMessage(Text.of(msg1, msg2));
             player.sendMessage(Text.of(TextColors.GREEN,
                     "You can right click the chest to see your progress. To begin, throw the necessary materials near the chest."));
         });
     }
 
-    private void rotateBlocks(JSONArray blocks, int iterations) {
-        blocks.forEach((obj) -> {
-            JSONObject block = (JSONObject) obj;
-            JSONObject pos = block.getJSONObject(POSITION);
-            for (int i = 0; i < iterations; i++) {
-                int tmpX = pos.getInt(POSITION_X);
-                pos.put(POSITION_X, pos.getInt(POSITION_Z));
-                pos.put(POSITION_Z, -tmpX);
-            }
-            if (iterations == 1 || iterations == 3) {
-                pos.put(POSITION_Z, -pos.getInt(POSITION_Z));
-                pos.put(POSITION_X, -pos.getInt(POSITION_X));
-            }
-        });
+    private void rotateBlockPosition(JSONObject block, int iterations) {
+        JSONObject pos = block.getJSONObject(POSITION);
+        for (int i = 0; i < iterations; i++) {
+            int tmpX = pos.getInt(POSITION_X);
+            pos.put(POSITION_X, pos.getInt(POSITION_Z));
+            pos.put(POSITION_Z, -tmpX);
+        }
+        if (iterations == 1 || iterations == 3) {
+            pos.put(POSITION_Z, -pos.getInt(POSITION_Z));
+            pos.put(POSITION_X, -pos.getInt(POSITION_X));
+        }
 
     }
+    
+    private void restoreBlockLocation(JSONObject block, Vector3i origin, UUID worldUUID) {
+    	JSONObject pos = block.getJSONObject(POSITION);
+        pos.put(POSITION_X, pos.getInt(POSITION_X) + origin.getX());
+        pos.put(POSITION_Y, pos.getInt(POSITION_Y) + origin.getY());
+        pos.put(POSITION_Z, pos.getInt(POSITION_Z) + origin.getZ());
+        block.put(WORLD_UUID, worldUUID.toString());
+    }
+    
+    private Optional<BlockSnapshot> deserializeBlock(JSONObject blockJson) {
+    	BufferedReader reader = new BufferedReader(new StringReader(blockJson.toString()));
+        JSONConfigurationLoader loader = JSONConfigurationLoader.builder().setSource(() -> {
+            return reader;
+        }).build();
+        ConfigurationNode node = null;
+        try {
+            node = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Sponge.getDataManager().deserialize(BlockSnapshot.class, ConfigurateTranslator.instance().translateFrom(node));
+    }
+    
+    private BlockSnapshot rotateBlockDirectionData(BlockSnapshot block, int rotationIterations) {
+    	if(!block.get(Keys.DIRECTION).isPresent()) {
+    		System.out.println("No direction");
+    		return block;
+    	}
+    	Direction orig = block.get(Keys.DIRECTION).get();
+    	System.out.println("Original");
+    	System.out.println(orig);
+		int index = -1;
+		if (CARDINAL_SET.indexOf(orig) + rotationIterations >= 4) {
+			index = CARDINAL_SET.size() - CARDINAL_SET.indexOf(orig) - rotationIterations;
+		} else {
+			index = CARDINAL_SET.indexOf(orig) + rotationIterations;
+		}
+		System.out.println("Index");
+		System.out.println(index);
+		Direction newDir = CARDINAL_SET.get(index);
+		System.out.println("newDir");
+		System.out.println(newDir);
+		return block.with(Keys.DIRECTION, newDir).get();
+	}
 }
