57918726dd765b3349f073b486b64776841c5ac1
Robert Herhold <reherhold@gmail.com>
2016-07-16 15:13:38 -0400

    Create block snapshots synchronously but upload async

--- src/main/java/me/reherhold/edifice/command/executor/SaveStructureExecutor.java
+++ src/main/java/me/reherhold/edifice/command/executor/SaveStructureExecutor.java
@@ -76,14 +76,80 @@ public class SaveStructureExecutor implements CommandExecutor {
 			return CommandResult.empty();
 		}
 
-		World w = this.plugin.getPlayerSelectedLocations().get(uuid).getLeft().getExtent();
+		World world = this.plugin.getPlayerSelectedLocations().get(uuid).getLeft().getExtent();
 		Vector3i loc1 = this.plugin.getPlayerSelectedLocations().get(uuid).getLeft().getBlockPosition();
 		Vector3i loc2 = this.plugin.getPlayerSelectedLocations().get(uuid).getRight().getBlockPosition();
 
-		Sponge.getScheduler()
-				.createTaskBuilder().execute(new SaveStructureRunnable(player,
-						(String) args.getOne("name").get(), w, loc1, loc2))
-				.async().name("Edifice - Submit Structure to REST API").submit(this.plugin);
+		final String structureName = (String) args.getOne("name").get();
+
+		int minX = Math.min(loc1.getX(), loc2.getX());
+		int maxX = Math.max(loc1.getX(), loc2.getX());
+		int minY = Math.min(loc1.getY(), loc2.getY());
+		int maxY = Math.max(loc1.getY(), loc2.getY());
+		int minZ = Math.min(loc1.getZ(), loc2.getZ());
+		int maxZ = Math.max(loc1.getZ(), loc2.getZ());
+
+		Direction direction = Direction.getClosestHorizontal(convertRotation(player.getRotation()), Division.CARDINAL);
+
+		JSONObject structure = new JSONObject().put(NAME, structureName)
+				.put(CREATOR_UUID, player.getUniqueId().toString()).put(WIDTH, maxX - minX + 1)
+				.put(LENGTH, maxZ - minZ + 1).put(HEIGHT, maxY - minY + 1).put(BLOCKS, new JSONArray())
+				.put(DIRECTION, direction.toString());
+
+		player.sendMessage(Text.of(TextColors.GREEN, "Analyzing the structure..."));
+
+		Vector3i bottomCorner = new Vector3i(0, 0, 0);
+		// Vector3i topCorner = new Vector3i(0, 0, 0);
+		switch (direction) {
+		case NORTH:
+			bottomCorner = new Vector3i(minX, minY, maxZ);
+			// topCorner = new Vector3i(maxX, maxY, minZ);
+			break;
+		case SOUTH:
+			bottomCorner = new Vector3i(maxX, minY, minZ);
+			// topCorner = new Vector3i(minX, maxY, maxZ);
+			break;
+		case EAST:
+			bottomCorner = new Vector3i(minX, minY, minZ);
+			// topCorner = new Vector3i(maxX, maxY, maxZ);
+			break;
+		case WEST:
+			bottomCorner = new Vector3i(maxX, minY, maxZ);
+			// topCorner = new Vector3i(minX, maxY, minZ);
+			break;
+		default:
+			player.sendMessage(Text.of(TextColors.RED, "Look in the direction facing the front of your structure."));
+			return CommandResult.empty();
+		}
+
+		for (int i = minX; i <= maxX; i++) {
+			for (int j = minY; j <= maxY; j++) {
+				for (int k = minZ; k <= maxZ; k++) {
+					StringWriter writer = new StringWriter();
+					BlockSnapshot block = world.createSnapshot(new Vector3i(i, j, k));
+					if (block.getState().getType() == BlockTypes.AIR) {
+						continue;
+					}
+					ConfigurationNode node = SimpleConfigurationNode.root();
+					ConfigurateTranslator.instance().translateContainerToData(node, block.toContainer());
+					try {
+						JSONConfigurationLoader.builder().build().saveInternal(node, writer);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					JSONObject jsonBlock = new JSONObject(writer.toString());
+					jsonBlock.remove(WORLD_UUID);
+					jsonBlock.getJSONObject(POSITION).put(POSITION_X, i - bottomCorner.getX())
+							.put(POSITION_Y, j - bottomCorner.getY()).put(POSITION_Z, k - bottomCorner.getZ());
+					structure.getJSONArray(BLOCKS).put(jsonBlock);
+				}
+			}
+		}
+
+		player.sendMessage(Text.of(TextColors.GREEN, "Uploading the structure..."));
+
+		Sponge.getScheduler().createTaskBuilder().execute(new SaveStructureRunnable(player, structure)).async()
+				.name("Edifice - Submit Structure to REST API").submit(this.plugin);
 
 		return CommandResult.success();
 	}
@@ -91,91 +157,15 @@ public class SaveStructureExecutor implements CommandExecutor {
 	class SaveStructureRunnable implements Runnable {
 
 		private Player player;
-		private String structureName;
-		private World world;
-		private Vector3i loc1;
-		private Vector3i loc2;
+		private JSONObject structure;
 
-		public SaveStructureRunnable(Player player, String structureName, World world, Vector3i loc1,
-				Vector3i loc2) {
+		public SaveStructureRunnable(Player player, JSONObject structure) {
 			this.player = player;
-			this.structureName = structureName;
-			this.world = world;
-			this.loc1 = loc1;
-			this.loc2 = loc2;
+			this.structure = structure;
 		}
 
 		public void run() {
-			int minX = Math.min(this.loc1.getX(), this.loc2.getX());
-			int maxX = Math.max(this.loc1.getX(), this.loc2.getX());
-			int minY = Math.min(this.loc1.getY(), this.loc2.getY());
-			int maxY = Math.max(this.loc1.getY(), this.loc2.getY());
-			int minZ = Math.min(this.loc1.getZ(), this.loc2.getZ());
-			int maxZ = Math.max(this.loc1.getZ(), this.loc2.getZ());
-
-			Direction direction = Direction.getClosestHorizontal(convertRotation(player.getRotation()),
-					Division.CARDINAL);
-
-			JSONObject structure = new JSONObject().put(NAME, this.structureName)
-					.put(CREATOR_UUID, this.player.getUniqueId().toString()).put(WIDTH, maxX - minX + 1)
-					.put(LENGTH, maxZ - minZ + 1).put(HEIGHT, maxY - minY + 1).put(BLOCKS, new JSONArray())
-					.put(DIRECTION, direction.toString());
-
-			player.sendMessage(Text.of(TextColors.GREEN, "Analyzing the structure..."));
-
-			Vector3i bottomCorner = new Vector3i(0, 0, 0);
-			// Vector3i topCorner = new Vector3i(0, 0, 0);
-			switch (direction) {
-			case NORTH:
-				bottomCorner = new Vector3i(minX, minY, maxZ);
-				// topCorner = new Vector3i(maxX, maxY, minZ);
-				break;
-			case SOUTH:
-				bottomCorner = new Vector3i(maxX, minY, minZ);
-				// topCorner = new Vector3i(minX, maxY, maxZ);
-				break;
-			case EAST:
-				bottomCorner = new Vector3i(minX, minY, minZ);
-				// topCorner = new Vector3i(maxX, maxY, maxZ);
-				break;
-			case WEST:
-				bottomCorner = new Vector3i(maxX, minY, maxZ);
-				// topCorner = new Vector3i(minX, maxY, minZ);
-				break;
-			default:
-				player.sendMessage(
-						Text.of(TextColors.RED, "Look in the direction facing the front of your structure."));
-				return;
-			}
-
-			for (int i = minX; i <= maxX; i++) {
-				for (int j = minY; j <= maxY; j++) {
-					for (int k = minZ; k <= maxZ; k++) {
-						StringWriter writer = new StringWriter();
-						BlockSnapshot block = this.world.createSnapshot(new Vector3i(i, j, k));
-						if (block.getState().getType() == BlockTypes.AIR) {
-							continue;
-						}
-						ConfigurationNode node = SimpleConfigurationNode.root();
-						ConfigurateTranslator.instance().translateContainerToData(node, block.toContainer());
-						try {
-							JSONConfigurationLoader.builder().build().saveInternal(node, writer);
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-						JSONObject jsonBlock = new JSONObject(writer.toString());
-						jsonBlock.remove(WORLD_UUID);
-						jsonBlock.getJSONObject(POSITION).put(POSITION_X, i - bottomCorner.getX())
-								.put(POSITION_Y, j - bottomCorner.getY()).put(POSITION_Z, k - bottomCorner.getZ());
-						structure.getJSONArray(BLOCKS).put(jsonBlock);
-					}
-				}
-			}
-
-			player.sendMessage(Text.of(TextColors.GREEN, "Uploading the structure..."));
-
-			WebTarget target = Edifice.restClient
-					.target(Edifice.config.getRestURI().toString() + "/structures");
+			WebTarget target = Edifice.restClient.target(Edifice.config.getRestURI().toString() + "/structures");
 			Response response;
 			try {
 				response = target.request().post(Entity.entity(structure.toString(), MediaType.APPLICATION_JSON_TYPE));
@@ -189,13 +179,12 @@ public class SaveStructureExecutor implements CommandExecutor {
 				JSONObject responseBody = new JSONObject(response.readEntity(String.class));
 				String structureID = responseBody.getString(ID);
 				this.player.sendMessage(Text.of(TextColors.GREEN, "You have successfully uploaded ", TextColors.GOLD,
-						this.structureName, TextColors.GREEN, "."));
+						structure.getString(NAME), TextColors.GREEN, "."));
 				try {
 					this.player.sendMessage(Text.of(TextColors.GREEN, "Click ",
 							Text.builder("here").color(TextColors.GOLD)
 									.onClick(TextActions.openUrl(
-											new URL(Edifice.config.getWebURI().toString()
-													+ "/create/" + structureID)))
+											new URL(Edifice.config.getWebURI().toString() + "/create/" + structureID)))
 									.build(),
 							TextColors.GREEN, " to finalize it with a screenshot."));
 				} catch (MalformedURLException e) {
@@ -208,12 +197,13 @@ public class SaveStructureExecutor implements CommandExecutor {
 			}
 		}
 
-		private Vector3d convertRotation(Vector3d rotation) {
-			double pitch = ((rotation.getX() + 90) * Math.PI) / 180;
-			double yaw = ((rotation.getY() + 90) * Math.PI) / 180;
-			Vector3d vector = new Vector3d(Math.sin(pitch) * Math.cos(yaw), 0, Math.sin(pitch) * Math.sin(yaw));
-			return vector;
-		}
+	}
+
+	private Vector3d convertRotation(Vector3d rotation) {
+		double pitch = ((rotation.getX() + 90) * Math.PI) / 180;
+		double yaw = ((rotation.getY() + 90) * Math.PI) / 180;
+		Vector3d vector = new Vector3d(Math.sin(pitch) * Math.cos(yaw), 0, Math.sin(pitch) * Math.sin(yaw));
+		return vector;
 	}
 
 }
